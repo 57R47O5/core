f02797aefdaf4f6941f6433e6a4944bc84ea97a6
Esteban <serafa99@hotmail.com>
2025-12-09 17:03:52 -0300

    carteles: implementación de paciente en el nuevo esquema

--- backend/apps/carteles/urls/paciente_urls.py
+++ backend/apps/carteles/urls/paciente_urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
+from rest_framework import routers
 from apps.carteles.views.paciente_view import (
-    paciente_detail,
-    paciente_list_create,
-    paciente_search,
-    PacienteNombreOptions
+    PacienteNombreOptions,
+    PacienteRestController
 )
 
 from django.urls import path
 
+router = routers.SimpleRouter()
+router.register(r'pacientes', PacienteRestController, 'pacientes')
+
 urlpatterns = [
-    path('', paciente_list_create, name='pacientes'),
-    path('<int:pk>/', paciente_detail, name='paciente-detail'),
-    path('buscar/', paciente_search, name='paciente-search'),
     path('options/', PacienteNombreOptions.as_view(), name='paciente-nombre-options'),
 ]
+
+urlpatterns += router.urls
+
+
 
