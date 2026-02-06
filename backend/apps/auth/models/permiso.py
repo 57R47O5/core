705050135584aa5a43725c1ec7fd7074a2253c3d
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-02-06 10:17:06 -0300

    main: Corrige modelos de permisos

--- backend/apps/auth/models/permiso.py
+++ backend/apps/auth/models/permiso.py
@@ -1,6 +1,16 @@
 from django.db import models
-from framework.models.basemodels import ConstantModel
+from framework.models.basemodels import ConstantModel, ConstantModelManager
 
+class PermisoManager(ConstantModelManager):
+    """
+    Manager que expone permisos como constantes del dominio.
+    Cada App debe heredar de este Manager
+    class PermisosBase(PermisoManager):
+        BASE_PERSONA_VIEW = Constant("base.persona.view")
+        BASE_PERSONA_EDIT = Constant("base.persona.edit")
+        BASE_EMPRESA_VIEW = Constant("base.empresa.view")
+    """
+    ...
 class Permiso(ConstantModel):
     """
     Permiso atómico del sistema.
@@ -12,7 +22,9 @@ class Permiso(ConstantModel):
     La identidad del permiso está dada por su `codigo`, no por su ID.
     """
 
+    objects = PermisoManager()
     class Meta:
+        managed = False
         db_table = "permiso"
         verbose_name = "Permiso"
         verbose_name_plural = "Permisos"
--- backend/apps/auth/models/rol.py
+++ backend/apps/auth/models/rol.py
@@ -1,49 +1,19 @@
 from django.db import models
+from framework.models.basemodels import Constant, ConstantModel, ConstantModelManager
 
-class Rol(models.Model):
+class RolManager(ConstantModelManager):
+    OWNER = Constant("owner")
+
+class Rol(ConstantModel):
     """
     Rol del sistema.
     Agrupa permisos.
     No contiene lógica de autorización.
     """
-
-    # Identidad del rol
-    code = models.CharField(
-        max_length=50,
-        unique=True,
-        help_text="Identificador estable del rol (ej: admin, user, readonly)"
-    )
-
-    name = models.CharField(
-        max_length=100,
-        help_text="Nombre legible del rol"
-    )
-
-    description = models.TextField(
-        blank=True,
-        null=True,
-        help_text="Descripción funcional del rol"
-    )
-
-    # Estado
-    is_system = models.BooleanField(
-        default=False,
-        help_text="Indica si es un rol del sistema (no editable por usuarios)"
-    )
-
-    is_active = models.BooleanField(
-        default=True,
-        help_text="Permite desactivar el rol sin eliminarlo"
-    )
-
-    # Auditoría
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
+    objects=RolManager()
+    
     class Meta:
         db_table = "rol"
+        managed=False
         verbose_name = "Rol"
         verbose_name_plural = "Roles"
-
-    def __str__(self) -> str:
-        return self.code
