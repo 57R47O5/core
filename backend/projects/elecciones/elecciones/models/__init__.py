e55d73c5f8487bd7e775b4af28513b894c1bf577
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-02-09 08:58:48 -0300

    main: Creamos proyecto elecciones

--- backend/projects/elecciones/elecciones/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .campana import Campana
-from .colaborador import Colaborador
-from .importbach import ImportBatch
-from .padron import Padron
-from .visita import Visita
-from .votante import Votante
