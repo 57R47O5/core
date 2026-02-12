6318516093ee348520bbdb625c929abedbe06738
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-02-12 14:15:28 -0300

    main: Corregimos actualización de permisos elecciones

--- backend/apps/elecciones/rest_controllers/visita_rest_controller.py
+++ backend/apps/elecciones/rest_controllers/visita_rest_controller.py
@@ -4,10 +4,7 @@ from framework.models.basemodels import Constant
 
 from framework.api.options import BaseOptionsAPIView
 from apps.elecciones.models.visita import Visita
-from apps.elecciones.serializers.visita_serializer import (
-    VisitaCreateSerializer,
-    VisitaUpdateSerializer,
-    VisitaRetrieveSerializer)
+
 from controllers.base.base_rest_controller import ModelRestController
 
 class PermisosVisita(PermisoGroup):
@@ -15,13 +12,10 @@ class PermisosVisita(PermisoGroup):
     CREATE=Constant("elecciones.visita.create")
     UPDATE=Constant("elecciones.visita.update")
     DESTROY=Constant("elecciones.visita.destroy")
-
+    RESULTADO_VISITA_VIEW=Constant("elecciones.resultado_visita.view")
 
 class VisitaRestController(ModelRestController):
     label = "Visita"
     model = Visita
-    url = "visita"
-    create_serializer = VisitaCreateSerializer
-    update_serializer = VisitaUpdateSerializer
-    retrieve_serializer = VisitaRetrieveSerializer    
+    url = "visita"  
     permisos = PermisosVisita
