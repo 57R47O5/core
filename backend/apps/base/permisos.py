a875e80e60f8f840da5d74e97b68d56f72f80430
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-02-12 08:33:48 -0300

    main: Actualiza permisos y roles base

--- backend/apps/base/permisos.py
+++ backend/apps/base/permisos.py
@@ -1,12 +1,20 @@
-from apps.auth.models.permiso import PermisoManager
+from apps.auth.models.permiso import PermisoManager, PermisoGroup
 from framework.models.basemodels import Constant
 
+class PermisosPersonaFisica(PermisoGroup):
+    VIEW=Constant("base.persona_fisica.view")
+    CREATE=Constant("base.persona_fisica.create")
+    UPDATE=Constant("base.persona_fisica.update")
+    DESTROY=Constant("base.persona_fisica.destroy")
+
+class PermisosPersonaJuridica(PermisoGroup):
+    VIEW=Constant("base.persona_juridica.view")
+    CREATE=Constant("base.persona_juridica.create")
+    UPDATE=Constant("base.persona_juridica.update")
+    DESTROY=Constant("base.persona_juridica.destroy")
+
 class BasePermisos(PermisoManager):
-    PERSONA_FISICA_VIEW=Constant("base.persona_fisica.view")
-    PERSONA_FISICA_CREATE=Constant("base.persona_fisica.create")
-    PERSONA_FISICA_UPDATE=Constant("base.persona_fisica.update")
-    PERSONA_FISICA_DESTROY=Constant("base.persona_fisica.destroy")
-    PERSONA_JURIDICA_VIEW=Constant("base.persona_juridica.view")
-    PERSONA_JURIDICA_CREATE=Constant("base.persona_juridica.create")
-    PERSONA_JURIDICA_UPDATE=Constant("base.persona_juridica.update")
-    PERSONA_JURIDICA_DESTROY=Constant("base.persona_juridica.destroy")
+    grupos = [
+        PermisosPersonaFisica,
+        PermisosPersonaJuridica
+    ]
