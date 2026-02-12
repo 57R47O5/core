1a76f2c5d8fb7361a1cc78ca7eab880bb3569d30
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-02-12 08:33:48 -0300

    main: Actualiza permisos y roles base

--- backend/apps/base/roles.py
+++ backend/apps/base/roles.py
@@ -1,22 +1,25 @@
 from framework.models.basemodels import Constant
 from auth.models.rol import RolManager
-from base.permisos import BasePermisos
+from base.permisos import (
+    PermisosPersonaFisica,
+    PermisosPersonaJuridica
+)
 
 #TODO: Asegurarnos de que esto funciona
 class BaseRoles(RolManager):    
     BASE_VIEWER = Constant(
         "base.viewer",
         permisos=[
-            BasePermisos.PERSONA_FISICA_VIEW,
-            BasePermisos.PERSONA_JURIDICA_VIEW,
+            PermisosPersonaFisica.VIEW,
+            PermisosPersonaJuridica.VIEW,
         ]
     )
 
     BASE_ADMIN = Constant(
         "base.admin",
         permisos=[
-            BasePermisos.PERSONA_FISICA_VIEW,
-            BasePermisos.PERSONA_JURIDICA_VIEW,
-            BasePermisos.PERSONA_FISICA_EDIT,
+            PermisosPersonaFisica.VIEW,
+            PermisosPersonaJuridica.VIEW,
+            PermisosPersonaJuridica.UPDATE,
         ]
     )
