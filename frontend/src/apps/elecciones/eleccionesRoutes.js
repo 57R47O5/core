d72f33c30eb8d302ab92cbce9be325d99af122f1
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-02-16 20:36:27 -0300

    main: Actualiza  routes

--- frontend/src/apps/elecciones/eleccionesRoutes.js
+++ frontend/src/apps/elecciones/eleccionesRoutes.js
@@ -1,18 +1,12 @@
 import campanaRoute from "./routes/campanaRoute";
-import ciclo_electoralRoute from "./routes/ciclo_electoralRoute";
 import colaboradorRoute from "./routes/colaboradorRoute";
-import estado_salidaRoute from "./routes/estado_salidaRoute";
-import resultado_visitaRoute from "./routes/resultado_visitaRoute";
 import salidaRoute from "./routes/salidaRoute";
 import seccionalRoute from "./routes/seccionalRoute";
 import votanteRoute from "./routes/votanteRoute";
 
 const eleccionesRoutes = [
     ...campanaRoute,
-    ...ciclo_electoralRoute,
     ...colaboradorRoute,
-    ...estado_salidaRoute,
-    ...resultado_visitaRoute,
     ...salidaRoute,
     ...seccionalRoute,
     ...votanteRoute,
