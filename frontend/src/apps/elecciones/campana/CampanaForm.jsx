2e97c4a99866dfb45cd7a0590ee6e7dd73d9c602
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-03-13 16:29:02 -0300

    main: Mejora interfaz
    Simplifica Select Formik
    
    Simplifica asociacion de persona-usuario

--- frontend/src/apps/elecciones/campana/CampanaForm.jsx
+++ frontend/src/apps/elecciones/campana/CampanaForm.jsx
@@ -1,29 +1,10 @@
 
-import { useMemo } from "react";
 import { useModelForm } from "../../../hooks/useModelForm";
 import { CampanaFields } from "./CampanaFields";
-import { useRouteMode } from "../../../hooks/useRouteMode";
 
 export default function CampanaForm() {
-  const { isEdit } = useRouteMode();
 
-  const dynamicFields = useMemo(() => ({
-    ...CampanaFields,
-    candidato: {
-      ...CampanaFields.candidato,
-      disabled: isEdit,
-    },
-    distrito: {
-      ...CampanaFields.distrito,
-      disabled: isEdit,
-    },
-    ciclo: {
-      ...CampanaFields.ciclo,
-      disabled: isEdit,
-    },
-  }), [isEdit]);
-
-  const {FormFields} = useModelForm(dynamicFields);
+  const {FormFields} = useModelForm(CampanaFields);
 
   return (
   <>
