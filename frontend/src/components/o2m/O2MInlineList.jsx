10c5733949a6aae1c88a7ca8aec1966467a039eb
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-04-01 00:05:26 -0300

    main: Correccion nombre o2minlinelist

---
+++ frontend/src/components/o2m/O2MInlineList.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import { useO2M } from "./O2MProvider";
+import { Spinner } from "react-bootstrap";
+import O2MTable from "./O2MTable";
+import { Card } from "react-bootstrap";
+import CenteredCard from "../displays/CenteredCard";
+import './../../styles/cards.css'
+
+export default function O2MInlineList({
+  title,
+  filtros,
+}) {
+  const {buscar, version} = useO2M();
+  const [items, setItems] = useState([]);
+  const [cargando, setCargando] = useState(true);
+
+  useEffect(() => {
+    setCargando(true);
+    buscar(filtros).then((data) => {
+      setItems(data);
+      setCargando(false);
+    });
+  }, [filtros, version]);
+
+  if (cargando) return <Spinner />;
+
+  return (
+    <CenteredCard title={title}>
+      <Card.Body>
+      <O2MTable
+        items={items}
+        />
+      </Card.Body>
+    </CenteredCard>
+  );
+}
