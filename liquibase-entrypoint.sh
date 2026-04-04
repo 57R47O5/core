8e5933293c6f26273972212b544842fc1c513124
Esteban Serafini <100873189+57R47O5@users.noreply.github.com>
2026-04-04 18:28:57 -0300

    main: Actualiza docker
    Para separar liquibase del flujo normal

---
+++ liquibase-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# Defaults
+CLASSPATH="/liquibase/classpath/postgresql-42.7.8.jar"
+CHANGELOG="/liquibase/changelog/generated/elecciones/master.yaml"
+URL="jdbc:postgresql://db:5432/${DB_NAME}"
+
+# Ejecutar liquibase con defaults + comando recibido
+liquibase \
+  --classpath=$CLASSPATH \
+  --url=$URL \
+  --username=$DB_USER \
+  --password=$DB_PASSWORD \
+  --changeLogFile=$CHANGELOG \
+  "$@"
